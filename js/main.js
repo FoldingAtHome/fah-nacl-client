d1e770cc42a8b17fac850b5fa9f3dc37c9a752cb
Joseph Coffland <joseph@cauldrondevelopment.com>
2013-12-10 13:56:54 +0800

    Added licensing tags

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+/*
+              This file is part of the Folding@home NaCl Client
+
+          Copyright (c) 2013, Hong Kong University Science & Technology
+                 Copyright (c) 2013, Stanford University
+                             All rights reserved.
+
+        This software is free software: you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+        as published by the Free Software Foundation, either version 2.1 of
+        the License, or (at your option) any later version.
+
+        This software is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public
+        License along with this software.  If not, see
+        <http://www.gnu.org/licenses/>.
+
+                For information regarding this software email:
+                               Joseph Coffland
+                        joseph@cauldrondevelopment.com
+*/
+
 var as_url = 'http://fah.stanford.edu:8888';
-var as_url = 'http://localhost:8888';
+//var as_url = 'http://localhost:8888';
 
 var fah = {
     version: '8.0.0',
